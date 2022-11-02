537949a4a48c7682ed67dd77121d4a8647835276
siddhant <sidalwaystrue@gmail.com>
2022-11-02 18:04:41 +0545

    working on another

--- src/components/login.js
+++ src/components/login.js
@@ -2,25 +2,33 @@ import React from 'react'
 // import MailOutlineIcon from '@material-ui/icons/MailOutlineIcon';
 // import InputAdornment from '@material-ui/material/InputAdornment';
 import Box from '@material-ui/core/Box';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import ThunderstormOutlinedIcon from '@mui/icons-material/ThunderstormOutlined';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MailOutlinedIcon from '@material-ui/icons/MailOutlined';
 import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Earbuds from "@material-ui/icons/Earbuds";
+// import Earbuds from '@mui/icons-material/Earbuds';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+
+// import LockIcon from '@mui/icons-material/Lock';
 // import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
 const Login=()=>{
 
-    const paperStyle={padding :20,height:'60vh',width:380, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const paperStyle={padding :'15px',height:'50vh',width:480, margin:"15px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e',marginTop:"100px"}
     const btnstyle={margin:'8px 0'}
-    const backgroundstyle={backgroundColor:'#F0F7FF',height:'100%',width:'100%'}
+    const backgroundstyle={backgroundColor:'#F0F7FF',height:'100vh'}
    
     return(
         <Grid style={backgroundstyle}>
-        <Grid align='center'><img id="logo" src="./logo.png" /></Grid>
+        <Grid align='center'> <img id="logo" style={{marginTop:"10vh"}} src="./logo.png" /></Grid>
             <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
+            
+                <Grid align='center' style={{padding:"20px"}}>
                 
                    
                     <h2 id="headingstyle">Welcome Back</h2>
@@ -28,18 +36,26 @@ const Login=()=>{
                 </Grid>
               
 
-                <Grid  container spacing={2}>
-                <Grid md={2} align='center'>
-
-                <LockOutlinedIcon />
+                <Grid  container spacing={3} style={{padding:"10px",borderLeft:'3px solid ',borderColor:'#1678F2'}}>
+               
+                   <Grid md={3} align="center"  >
+                    <Grid><MailOutlinedIcon fontSize="large" style={{minHeight:"6vh",color:'grey'}}/></Grid>
+                  <Grid>
+                  <Grid align="center"  style={{font:"Poppins",color:'grey',fontSize: '12px'}} >Login with Email</Grid>
+               
+         
+                  </Grid>
+                   
     
-                 </Grid>
+                    </Grid> 
+             
+          
                   
-                <Grid md={10} mt={10}>
-                <TextField  placeholder='Enter Your Mail'  InputProps={{
+                <Grid md={9} align="right">
+                <TextField size="small"  placeholder='Enter Your Email'  InputProps={{
           startAdornment: (
             <InputAdornment position="start">
-              <MailOutlinedIcon />
+              <MailOutlinedIcon fontSize="small"/>
             </InputAdornment>
           ),
         }} variant="outlined" fullWidth required />
@@ -48,39 +64,45 @@ const Login=()=>{
                  </Grid>
                  </Grid>
               
+                 <Grid  container spacing={3} style={{padding:"10px"}}>
                
+               <Grid md={3} align="center">
+                <Grid >
+               <ThunderstormOutlinedIcon fontSize="large"  style={{minHeight:"6vh",color:'Grey'}}/>
+               </Grid>
+
+               <Grid align="center"  style={{font:"Poppins",color:'grey',fontSize: '12px'}} >Login with SSO</Grid>
+                </Grid> 
+         
+      
+              
+            <Grid md={9}>
+            <TextField size="small"   placeholder='Enter Your Password'  InputProps={{
+      startAdornment: (
+        <InputAdornment position="start">
+          <LockOutlinedIcon  fontSize="small" />
+        </InputAdornment>
+      ),
+    }} variant="outlined" fullWidth required />
+
+
+             </Grid>
+             </Grid>
                 
+                <Grid container spacing={3} style={{padding:"10px"}}>
+                 <Grid md={3} align="center" >
+                 
+                 </Grid>
+                 <Grid md={9}>
+                 <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                  </Grid>
                 
-            
-                <TextField  placeholder='Enter Your Password' type='password' InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <LockOutlinedIcon />
-            </InputAdornment>
-          ),
-        }} fullWidth required/>
-                <FormControlLabel
-                    control={
-                    <Checkbox
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label="Remember me"
-                 />
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography >
-                     <Link href="#" >
-                        Forgot password ?
-                </Link>
-                </Typography>
-                <Typography > Do you have an account ?
-                     <Link href="#" >
-                        Sign Up 
-                </Link>
-                </Typography>
+                </Grid>
+          
+                
+               
             </Paper>
-        </Grid>
+            <Grid align="center" style={{fontFamily:'Helvetica Neue LT Std',fontWeight:"500",fontSize:'16px',color:'black',paddingTop:'3vh'}}><span style={{fontFamily:'Poppins',fontStyle:'Normal',marginTop:'100px'}}>Forgot Your Password ? <a href="*">Reset Password</a></span></Grid></Grid>
     )
 }
 
